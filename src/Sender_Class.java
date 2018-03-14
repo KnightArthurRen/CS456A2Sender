5bce74bcdcf892f9b56a2608cd6fd7e8f809afe1
Arthur <renyirw@gmail.com>
2018-03-13 20:16:15 -0400

    Clean up

--- src/Sender_Class.java
+++ src/Sender_Class.java
@@ -37,35 +37,27 @@ public class Sender_Class {
 
         @Override
         public void run() {
-            System.out.println("receive thread called");
             while(true) { // and waiting for all sended
 //                Received the package
                 try {
-                    System.out.println("waiting fo ack");
                     socket.receive(buffer);
-                    System.out.println("received ack");
                 } catch (java.io.IOException e) {
                     System.err.println("Sender_Class: Receiving package failed!");
                 }
                 try {
                     received_packet = packet.parseUDPdata(buffer.getData());
-                    System.out.println("parsed ack");
                 } catch (java.lang.Exception e) {
                     System.err.println("Sender_Class: Received package cannot be parsed");
                 }
 
 //                Check the output
 //                If the package is EOT
-                System.out.print("the type is ");
-                System.out.println(received_packet.getType());
                 if(received_packet.getType() == 2) {
-                    System.out.println("EOT received!");
                     Sender_Class.this.queue_lock.lock();
                     Sender_Class.this.shutdown = true;
                     Sender_Class.this.queue_lock.unlock();
                     break;
                 } else {
-                    System.out.println("Not EOT!");
                 }
 
 //                Record the log
@@ -77,25 +69,18 @@ public class Sender_Class {
                     System.err.println("Sender_Class: failed to log ack");
                 }
 
-                System.out.println("Log completed");
                 Sender_Class.this.queue_lock.lock();
 //                First loop check if the ack is duplicate or not
-                System.out.println("acquired lock");
                 boolean duplicate = true;
-                System.out.println("set duplicate to be true");
                 int i = 0;
                 for(; i < Sender_Class.this.UnACKQueue.size(); i++) {
-                    System.out.println("what's wrong");
                     if(Sender_Class.this.UnACKQueue.get(i).getSeqNum() == received_packet.getSeqNum()) {
                         duplicate = false;
                         break;
                     }
                 }
 //                if it's not a duplicate, ack everything up to the ack seq_num
-                System.out.println("checking duplicate");
                 if (!duplicate){
-                    System.out.print("not duplicate ack, ack is ");
-                    System.out.println(i);
                     if(i == 0) {
                         Sender_Class.this.UnACKQueue.remove(i);
                         Sender_Class.this.timer.remove(i);
@@ -103,9 +88,7 @@ public class Sender_Class {
                         Sender_Class.this.UnACKQueue.subList(0,i).clear();
                         Sender_Class.this.timer.subList(0,i).clear();
                     }
-                    System.out.println("cleaned the queue due to ack");
                 } else {
-                    System.out.println("duplicate ack!");
                 }
                 Sender_Class.this.queue_lock.unlock();
             }
@@ -124,7 +107,6 @@ public class Sender_Class {
         }
         private void send_pkg(packet p){
             //                        send the package
-//            System.out.println("Started to send");
             Sender_Class.this.UnACKQueue.add(new packet(p));
             DatagramPacket binary = new DatagramPacket(p.getUDPdata(),p.getUDPdata().length, emulator_ip,emulator_port);
             try{
@@ -142,11 +124,9 @@ public class Sender_Class {
             } catch (java.io.IOException e) {
                 System.err.println("Sender_Class: failed to log seqnum");
             }
-//            System.out.println("package sented");
         }
         @Override
         public void run() {
-//            System.out.println("sender thread called");
             content_length = Sender_Class.this.contents.length();
 //                Parse the entire content and send it with 500 character chunks each and send
             for(int index = 0; index < content_length; index += 500) {
@@ -162,22 +142,13 @@ public class Sender_Class {
                 } catch (java.lang.Exception e) {
                     System.err.println("Sender_Class: create new packet failed!");
                 }
-//                System.out.println("Package constructed");
                 while(true) {
                     Sender_Class.this.queue_lock.lock();
-                    if(Sender_Class.this.timer.size() != 0 && false) {
-                        System.out.println("attempt to send packet");
-                        System.out.println("current time is");
-                        System.out.println(System.nanoTime());
-                        System.out.println("timeout time is");
-                        System.out.println(Sender_Class.this.timer.get(0));
-                    }
                     if(Sender_Class.this.UnACKQueue.size() < 10) {
                         send_pkg(new_packet);
                         Sender_Class.this.queue_lock.unlock();
                         break;
                     } else if(System.nanoTime() - Sender_Class.this.timer.get(0) > timeout) {
-//                        System.out.println("over timeout, resent the entire queue");
                         //                    If there is one time out, resend all after timeout
                         Sender_Class.this.timer.clear();
                         List<packet> ResentList = new ArrayList<>();
@@ -198,10 +169,7 @@ public class Sender_Class {
 //                If all is acked, proceed
                 if (Sender_Class.this.UnACKQueue.size() == 0) break;
                 if(System.nanoTime() - Sender_Class.this.timer.get(0) > timeout) {
-//                    System.out.println("all pacakge send, over timeout, resent the entire queue");
-//                    System.out.print("send package is ");
                     for(packet p : Sender_Class.this.UnACKQueue) {
-//                        System.out.println(p.getSeqNum());
                     }
                     //                    If there is one time out, resend all after timeout
                     List<packet> ResentList = new ArrayList<>();
@@ -218,9 +186,6 @@ public class Sender_Class {
             }
 //            After all the packages are send, send the EOT
             try{
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                System.out.println("EOT send!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 new_packet = packet.createEOT(seqnum % 32);
                 long eot_timer = System.nanoTime();
                 DatagramPacket binary = new DatagramPacket(new_packet.getUDPdata(),new_packet.getUDPdata().length, emulator_ip,emulator_port);
@@ -255,7 +220,6 @@ public class Sender_Class {
         this.UnACKQueue = new ArrayList<>(10);
         this.timer = new ArrayList<>();
         this.timeout =timeout;
-        System.out.println("Constructor called");
         try {
             socket = new DatagramSocket(Sender_Class_receive_port);
         } catch (java.net.SocketException e) {
@@ -278,7 +242,6 @@ public class Sender_Class {
             System.err.println("Sender_Class: failed to create log files");
         }
 //        Create the threads
-        System.out.println("Thread construction called");
         Receiving = new Receiving(socket);
         Thread receive = new Thread(Receiving);
         receive.start();
